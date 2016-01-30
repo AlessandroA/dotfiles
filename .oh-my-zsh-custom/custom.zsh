3376bf4a07029cb03efd1a25731026515efe39c9
Alessandro Angelino <alessandro@angelino.io>
2016-01-30 23:29:50 +0000

    Add alias for experimental virtualenv

--- .oh-my-zsh-custom/custom.zsh
+++ .oh-my-zsh-custom/custom.zsh
@@ -58,7 +58,11 @@ alias varm="pydoc () { } && source $VIRTENVS_DIR/ARMmbed/bin/activate"
 
 # personal virtual environment
 # the pydoc thingie fixes an error message on a non-existing alias
-alias vpers="pydoc () { } && source $VIRTENVS_DIR/personal/bin/activate"
+alias vper="pydoc () { } && source $VIRTENVS_DIR/personal/bin/activate"
+
+# experimental virtual environment
+# the pydoc thingie fixes an error message on a non-existing alias
+alias vexp="pydoc () { } && source $VIRTENVS_DIR/experimental/bin/activate"
 
 # go to some frequent places
 alias arm="cd ~/code/ARMmbed"
@@ -66,6 +70,7 @@ alias ale="cd ~/code/AlessandroA"
 alias u="cd ~/code/ARMmbed/uvisor"
 alias ulib="cd ~/code/ARMmbed/uvisor-lib"
 alias uhw="cd ~/code/ARMmbed/uvisor-helloworld"
+alias ult="cd ~/code/ARMmbed/uvisor-lib-tests"
 
 ################################################################################
 # python and pip
